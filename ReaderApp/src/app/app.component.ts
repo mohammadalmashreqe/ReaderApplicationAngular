0eca698e5e74cf3b190d69521165533e1c485007
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-04-28 13:28:42 +0300

    Complete

--- ReaderApp/src/app/app.component.ts
+++ ReaderApp/src/app/app.component.ts
@@ -9,55 +9,92 @@ import { identifierModuleUrl } from '@angular/compiler';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   * Page title of app component
+   */
   pageTitle: string = 'Reader';
+  /**
+   * List  of app component
+   */
   List: IAttributes[] = [];
+  /**
+   * Mydata  of app component
+   */
   mydata: string;
+  /**
+   * Error message of app component
+   */
   errorMessage = '';
+  /**
+   * Result  of app component
+   */
+  Result: any[] = [];
+  /**
+   * Creates an instance of app component.
+   * @param service 
+   */
   constructor(private service: GetConfigService) {
 
 
-  
+    try {
 
-    this.service.getConfig().subscribe(
-      products => {
-        this.List = products;
+      this.service.getConfig().subscribe(
+        products => {
+          this.List = products;
 
-      },
-      error => this.errorMessage = <any>error
-    );
+        },
+        error => this.errorMessage = <any>error
+      );
 
-
-    
-    
-
-
-
-  }
-  Read(datavalidator:boolean): void {
-    var arr= new Array(this.List.length);
-
-    if(!datavalidator)
-    {
-     
-      var result= this.mydata.split(' ');
-
-      for (var i=0;i<this.List.length;i++)
-      { var regualrExp= new RegExp(this.List[i].regularExpression);
-        for(var j=0;j<result.length;j++)
-        {
-         
-          if(regualrExp.test(result[j]))
-          {
-            arr.push(result[j]);
-          }
-          
-        }
-      }
-      console.log(arr);
 
     }
-    
+    catch (err) {
+      console.log(err);
+    }
+
+
+
 
 
   }
+
+  /**
+   * Reads app component
+   * @param datavalidator 
+   */
+  Read(datavalidator: boolean): void {
+    try { if (!datavalidator) {
+      var spiltdata = this.mydata.split(' ');
+      var tempList = new Array();
+
+     
+
+        for (var i = 0; i < this.List.length; i++) {
+          var temp = new Array();
+          temp.push(this.List[i].name);
+          for (var j = 0; j < spiltdata.length; j++) {
+            var regualr = new RegExp(this.List[i].regularExpression)
+            if (regualr.test(spiltdata[j])) {
+              temp.push(spiltdata[j]);
+            }
+
+          }
+          tempList.push(temp);
+
+
+
+        }
+        this.Result = tempList;
+
+        console.log(this.Result);
+      }
+
+
+
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
 }
